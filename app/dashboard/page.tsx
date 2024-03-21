5471d21153a1f7bf07d442d0a267f5fea0bc0068
luke7231 <tmddnjs7231@gmail.com>
2024-03-21 22:47:33 +0900

    feat: ~tutorial9 complete ('data fetching', 'static vs dynamic', 'streaming')

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-  return <p> Hello world </p>;
+import { Suspense } from 'react';
+import { fetchCardData, fetchLatestInvoices } from '../lib/data';
+import CardWrapper, { Card } from '../ui/dashboard/cards';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+
+import { lusitana } from '../ui/fonts';
+import { CardsSkeleton, RevenueChartSkeleton } from '../ui/skeletons';
+
+export default async function Page() {
+  const latestInvoices = await fetchLatestInvoices();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+        <Suspense fallback={'Loading'}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
